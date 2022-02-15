5448b534698c1a26ac78d7e51723b4ebe436a5a8
sanilborkar <8661410+sanilborkar@users.noreply.github.com>
2022-02-15 18:00:20 +0530

    Added personal and dummy info

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -18,6 +18,7 @@ const StyledAboutSection = styled.section`
     }
   }
 `;
+
 const StyledText = styled.div`
   ul.skills-list {
     display: grid;
@@ -46,6 +47,7 @@ const StyledText = styled.div`
     }
   }
 `;
+
 const StyledPic = styled.div`
   position: relative;
   max-width: 300px;
@@ -126,7 +128,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['GoLang', 'Java', 'Python', 'ReactJS', 'NodeJS', 'GatsbyJS'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -136,30 +138,23 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Sanil and I enjoy programming. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+              ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out
+              print, graphic or web designs. The passage is attributed to an unknown typesetter in
+              the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum
+              et Malorum for use in a type specimen book. It usually begins with
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              All of this lorem ipsum text was taken from{' '}
+              <a href="https://loremipsum.io/">Lorem Ipsum</a> website.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,16 +59,15 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Sanil Sinai Borkar.</h2>;
+  const three = <h3 className="big-heading">I build software products.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a software engineer specializing creating new products. Currently, I’m focused on
+        building some exciting e-commerce products at{' '}
+        <a href="https://example.com/" target="_blank" rel="noreferrer">
+          My current company
         </a>
         .
       </p>
@@ -77,7 +76,7 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="https://www.example.com/courses"
       target="_blank"
       rel="noreferrer">
       Check out my course!
