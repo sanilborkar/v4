4e14f1974efe4cf8b468422b063ad8abf9f1ab2b
sanilborkar <8661410+sanilborkar@users.noreply.github.com>
2022-03-04 22:37:55 +0530

    Reverted deleting posts/tag pages and content

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,20 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: `${__dirname}/content/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/content/posts`,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
